824193f49608eb42409cd2113d7160f93cc002aa
Raju Masakapalli <masakapalliraju971@gmail.com>
2025-12-13 17:20:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6829288690-2c203",
+  "appId": "1:672501063100:web:3ddc8d116174ddc7e55fb9",
+  "apiKey": "AIzaSyAhncyhst1L6Gts7BP_E4kaSNnOQJm2LbI",
+  "authDomain": "studio-6829288690-2c203.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "672501063100"
+};
